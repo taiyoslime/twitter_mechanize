13414aaebbd365b7ccc5bb2d43a0691634b8d98a
taiyoslime <hase.tekitou@gmail.com>
2016-12-29 20:58:52 +0900

    fix typo 2

--- lib/twitter_mechanize.rb
+++ lib/twitter_mechanize.rb
@@ -5,8 +5,8 @@ require_relative "twitter_mechanize/agent"
 module TwitterMechanize
 	module_function
 	def init id,password
-		agent = Agent.new(id,password)
-		raise AuthError,"Authorization failed."unless agent.isLoggedin?
+		agent = Agent.new id,password
+		raise AuthError,"Authorization failed." unless agent.isLoggedin?
 		agent
 	end
 end
--- lib/twitter_mechanize/agent.rb
+++ lib/twitter_mechanize/agent.rb
@@ -4,11 +4,11 @@ require_relative "errors "
 module TwitterMechanize
 	class Agent
 		def initialize id,password
-			@agent = auth(id,password)
+			@agent = auth id,password
 		end
 
 		def tweet text
-			page = @agent.get("https://twitter.com/intent/tweet?text=#{text}")
+			page = @agent.get "https://twitter.com/intent/tweet?text=#{text}"
 			form = page.form_with(action:"/intent/tweet")
 			@agent.submit form
 			returi = @agent.page.uri
@@ -17,7 +17,7 @@ module TwitterMechanize
 		end
 
 		def isLoggedin?
-			@agent.get("https://twitter.com/login/")
+			@agent.get "https://twitter.com/login/"
 			@agent.page.uri.to_s == "https://twitter.com/" ? true : false
 		end
 
@@ -27,7 +27,7 @@ module TwitterMechanize
 			agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 			agent.follow_meta_refresh = true
 			agent.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
-			page = agent.get("https://twitter.com/login/")
+			page = agent.get "https://twitter.com/login/"
 			form = page.form_with(action:"https://twitter.com/sessions")
 			form.field_with(name:"session[username_or_email]").value = id
 			form.field_with(name:"session[password]").value = password
